f1c6185129912300e29d97bd5deb11fd0dacb3c8
IxquitilisSaid <binaryspell@gmail.com>
2019-03-06 19:11:17 +0100

    done for now. *clap clap* NEXT. MEME

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,12 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    }
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {}
--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipesService } from '../recipes.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,14 +14,19 @@ export class RecipeDetailPage implements OnInit {
 
   // Activated Route is built-in from angular router
   // contains info about the loaded component
-  constructor(private activatedRoute: ActivatedRoute, private recipesService: RecipesService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipesService: RecipesService,
+    private router: Router,
+    private alertCtrl: AlertController
+  ) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
-
       // check if observable contains 'recipeId' param
-      // if not then return the page (still needs the logic)
+      // if not then return to home
       if (!paramMap.has('recipeId')) {
+        this.router.navigate(['/recipes']);
         return;
       }
 
@@ -30,4 +36,26 @@ export class RecipeDetailPage implements OnInit {
     });
   }
 
+  onDeleteRecipe() {
+    this.alertCtrl.create({
+      header: 'Are you sure?',
+      message: 'Do you really want to delete the recipe?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.recipesService.deleteRecipe(this.loadedRecipe.id);
+            this.router.navigate(['/recipes']);
+          }
+        }
+      ]
+    })
+    .then(alertEl => {
+      alertEl.present();
+    });
+  }
 }
